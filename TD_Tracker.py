1cba8f645a35049843ae44e9204b0da2e2bc9910
TrustyTFox <52539777+TrustyTFox@users.noreply.github.com>
2022-03-13 12:52:02 +0100

    Clamped match history to ranked fives improved readability when iterating through matches with break statements Added support for multiple player stats

--- TD_Tracker.py
+++ TD_Tracker.py
@@ -20,12 +20,20 @@ class TDTrackerV02:
         cas.set_riot_api_key(config.api_key)
         # summoner vars
         self.summoner_object = cas.Summoner(name=self.summoner_name, region=cas.data.Region("EUW"))
+        self.summoner_history = cas.MatchHistory(continent=cas.data.Continent("EUROPE"),
+                                                 puuid=self.summoner_object.puuid,
+                                                 queue=cas.Queue.ranked_solo_fives, begin_index=0, end_index=2)
 
     def run(self):
         # setup vars
         player_info = []
         # load from json
-        match_info = json.load(open('all_match_info.json'))
+        match_info = {}
+        try:
+            match_info = json.load(open('all_match_info.json'))
+            match_info = match_info[self.summoner_name]
+        except KeyError:
+            pass
 
         # helper methods
         def calc_winrate(part):
@@ -42,35 +50,45 @@ class TDTrackerV02:
             return rank
 
         # iterate through match history
-        for i, match in enumerate(self.summoner_object.match_history):
+        for i, match in enumerate(self.summoner_history):
             # idk why the match.id gets formatted before reaching the bottom, setting it here
             current_match_id = match.id
 
-            # limit match amount
-            if i < self.match_amount:
+            # limit matches
+            if i == self.match_amount:
+                break
 
-                # look for match_id in json
-                if current_match_id not in match_info:
+            # look for match_id in json
+            if current_match_id in match_info:
+                break
 
-                    # iterate through participants in the match
-                    for participant in match.participants:
-                        try:
-                            player_info.append({
-                                'name': participant.summoner.name,
-                                'rank': format_rank(participant),
-                                'winrate': calc_winrate(participant),
-                            })
-                        except ValueError:
-                            player_info.append({
-                                'name': participant.summoner.name,
-                                'rank': 'UNRANKED',
-                                'winrate': 0,
-                            })
+            # iterate through participants in the match
+            for j, participant in enumerate(match.participants):
 
-                    # add to match list
-                    match_info[current_match_id] = player_info
-                else:
-                    print("Match found, skipping...")
+                # enforce limit, sometimes more than 10 participants are loaded
+                if j > 10:
+                    break
+
+                # check in player has ranked stats
+                try:
+
+                    player_info.append({
+                        'name': participant.summoner.name,
+                        'rank': format_rank(participant),
+                        'winrate': calc_winrate(participant),
+                    })
+
+                # add unranked if stats don't exist
+                except:
+
+                    player_info.append({
+                        'name': participant.summoner.name,
+                        'rank': 'UNRANKED',
+                        'winrate': 0,
+                    })
+
+            # add to match list
+            match_info[self.summoner_name] = {current_match_id: player_info}
 
         # write to json
         write_to_json(resources_path, 'all_match_info', match_info)
@@ -85,5 +103,9 @@ class TDTrackerV02:
 
 if __name__ == "__main__":
     # noinspection SpellCheckingInspection
-    match_jacana = TDTrackerV02("TURBO JACANA", 3)
-    match_jacana.run()
+    # setup players
+    matches = [TDTrackerV02("TURBO JACANA", 1), TDTrackerV02("TURBO Trusty", 1), TDTrackerV02("TURBO ALUCO", 1)]
+    # run all players
+    matches[0].empty_json()
+    for player in matches:
+        player.run()
